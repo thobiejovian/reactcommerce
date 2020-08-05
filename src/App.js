3ca3fb2640f48a77b3cab5c9d2ffafceb240ce5c
thobiejovian <jovian.thobie@gmail.com>
2020-08-05 15:13:46 +0200

    Shop Layout and Sign-in Page incl. Style finished

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/signinandsignup/signinandsignup.component';
+
+
 
 function App() {
-  return <div>
-    <HomePage />
-    </div>;
+  return(
+  <div>
+    <Header />
+    <Switch>
+       <Route exact path='/' component={HomePage} />
+       <Route path='/shop' component={ShopPage} />
+       <Route path='/signin' component={SignInAndSignUpPage} />
+     </Switch>
+  </div>);
 }
 
 export default App;
---
+++ src/components/preview-collection/preview-collection.component.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import CollectionItem from '../collection-item/collection-item.component'
+
+
+import './preview-collection.style.scss';
+
+
+const PreviewCollection = ({title, items}) => (
+  <div className='collection-preview'>
+    <h1 className='title'>{title.toUpperCase()}</h1>
+    <div className='preview'>
+
+    {
+      items.filter((item,idx) => idx < 4).map(({id, ...otherItemsProps}) => (
+        <CollectionItem key={id} {...otherItemsProps}/>
+      ))
+    }
+
+    </div>
+  </div>
+)
+
+export default PreviewCollection;
